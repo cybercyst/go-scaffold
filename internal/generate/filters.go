d7f4bf0bfe00c295a6140b33398b8a0537c62d4d
Forrest Loomis <forrest.loomis@docker.com>
2023-02-03 09:34:54 -0500

    feat(generate): add yaml filter

--- internal/generate/filters.go
+++ internal/generate/filters.go
@@ -3,12 +3,26 @@ package generate
 import (
 	"github.com/extemporalgenome/slug"
 	"github.com/flosch/pongo2/v6"
+	"gopkg.in/yaml.v3"
 )
 
 func initFilters() {
 	pongo2.RegisterFilter("slugify", filterSlugify)
+	pongo2.RegisterFilter("yaml", filterYaml)
 }
 
 func filterSlugify(in *pongo2.Value, param *pongo2.Value) (*pongo2.Value, *pongo2.Error) {
 	return pongo2.AsValue(slug.Slug(in.String())), nil
+}
+
+func filterYaml(in *pongo2.Value, param *pongo2.Value) (*pongo2.Value, *pongo2.Error) {
+	yamlBytes, err := yaml.Marshal(in.Interface())
+	if err != nil {
+		return nil, &pongo2.Error{
+			Sender:    "filter:yaml",
+			OrigError: err,
+		}
+	}
+
+	return pongo2.AsValue(string(yamlBytes)), nil
 }
