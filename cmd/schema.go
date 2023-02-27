efc17ee6817ac35273d8e612fa8774e28b07f063
Forrest Loomis <forrest.loomis@docker.com>
2023-02-27 10:53:25 -0500

    chore(schema): clean up schema cmd

--- cmd/schema.go
+++ cmd/schema.go
@@ -8,16 +8,12 @@ import (
 	"github.com/spf13/cobra"
 )
 
-// schemaCmd represents the schema command
 var schemaCmd = &cobra.Command{
 	Use:   "schema [URI]",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "Get schema for the provided template",
+	Long: `Return the schema for the defined template in JSON format.
+This can be used by other tooling in order to provide a rich user
+interface to collect the required input.`,
 	Run: func(cmd *cobra.Command, args []string) {
 		uri := args[0]
 
@@ -37,14 +33,4 @@ to quickly create a Cobra application.`,
 
 func init() {
 	rootCmd.AddCommand(schemaCmd)
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// schemaCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// schemaCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
